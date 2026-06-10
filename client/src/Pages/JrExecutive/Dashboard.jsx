89288b974bfb26d15a7cab4dac5e0695883bbc4e
Iksha Malhotra <iksha16005@gmail.com>
2026-06-10 18:48:19 +0530

    Updates for deployment

--- client/src/Pages/JrExecutive/Dashboard.jsx
+++ client/src/Pages/JrExecutive/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Layout from "../../components/Layout";
+import Layout from "../../Components/Layout";
 import { maintenanceAPI, handleAPIError } from '../../utils/Api';
 import {
   CheckCircle2,
